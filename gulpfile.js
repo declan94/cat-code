50c87a79bd6bec75f3794798eb91ef1116c86474
陈晔 <chenye94@qq.com>
2018-02-02 16:46:54 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minimist = require('minimist');
+var rimraf = require('rimraf');
+var runSequence = require('run-sequence');
+
+var knownOptions = {
+  string: ["folder", "output"]
+};
+
+var options = minimist(process.argv.slice(1), knownOptions);
+var output = options.output;
+if (!output) {
+  output = "./output"
+}
+
+gulp.task('clean', function(cb) {
+  rimraf(output, cb);
+});
+
+var gulpConcatExt = function(ext) {
+  var taskFunc = function() {
+    gulp.src([options.folder + "/**/*." + ext,
+        "!" + options.folder + "/**/bower_components/**/*",
+        "!" + options.folder + "/**/node_modules/**/*",
+        "!" + options.folder + "/**/dist/**/*"
+      ])
+      .pipe(concat("concat." + ext))
+      .pipe(gulp.dest(output));
+  }
+  return taskFunc;
+}
+
+var exts = ['js', 'java', 'html', 'css', 'less', 'py', 'sh', 'jade'];
+
+for (var i = 0; i < exts.length; i++) {
+  var ext = exts[i];
+  gulp.task(ext, gulpConcatExt(ext));
+}
+
+gulp.task('all', exts);
+
+gulp.task('default', function() {
+  runSequence(['clean', 'all']);
+});
